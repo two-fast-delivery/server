2d13af431987cbbe31f0abce66d3525392e5cf4b
hhegi <larbillilith@gmail.com>
2026-03-04 16:59:46 +0900

    feat: add Store entity

---
+++ src/main/java/nbcamp/TwoFastDelivery/domain/store/domain/Category.java
@@ -0,0 +1,21 @@
+package nbcamp.TwoFastDelivery.domain.store.domain;
+
+import java.util.UUID;
+import jakarta.persistence.*;
+
+
+
+
+
+/* 임시
+@Entity
+@Table(name = "p_category")
+public class Category {
+    @Id
+    @Column(name = "id", nullable = false, columnDefinition = "uuid")
+    private UUID id;
+
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(50)")
+    private String name;
+}
+*/
