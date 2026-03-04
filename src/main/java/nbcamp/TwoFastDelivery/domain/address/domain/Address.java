7fe3b646c394d42c05eb4761f24c14e1890f6738
ddangme <ssilver121@naver.com>
2026-03-04 15:51:18 +0900

    feat: Implement Address domain entity and AddressId value object

---
+++ src/main/java/nbcamp/TwoFastDelivery/domain/address/domain/Address.java
@@ -0,0 +1,59 @@
+package nbcamp.TwoFastDelivery.domain.address.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import nbcamp.TwoFastDelivery.global.common.BaseEntity;
+import org.springframework.util.Assert;
+
+import java.util.UUID;
+
+@Entity
+@ToString @Getter
+@Table(name = "P_ADDRESS")
+@Access(AccessType.FIELD)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address extends BaseEntity {
+
+    @EmbeddedId
+    private AddressId id;
+
+    @Column(nullable = false)
+    private String alias;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String detailAddress;
+
+    @Builder
+    public Address(UUID addressId, String alias, String address, String detailAddress) {
+        checkValidAddress(alias, address, detailAddress);
+
+        this.id = initializedId(addressId);
+        this.alias = alias;
+        this.address = address;
+        this.detailAddress = detailAddress;
+    }
+
+    public void update(String alias, String address, String detailAddress) {
+        checkValidAddress(alias, address, detailAddress);
+        this.alias = alias;
+        this.address = address;
+        this.detailAddress = detailAddress;
+    }
+
+    private AddressId initializedId(UUID addressId) {
+        if (addressId == null) {
+            return AddressId.of();
+        }
+
+        return AddressId.of(addressId);
+    }
+
+    private void checkValidAddress(String alias, String address, String detailAddress) {
+        Assert.hasText(alias, "배송지 별칭은 필수입니다.");
+        Assert.hasText(address, "기본 주소는 필수입니다.");
+        Assert.hasText(detailAddress, "상세 주소는 필수입니다.");
+    }
+}
