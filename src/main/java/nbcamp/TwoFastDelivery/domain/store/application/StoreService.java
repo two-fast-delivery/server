f6880db5ee6af2cb375ddab7e7a31ea368944ba4
hhegi <larbillilith@gmail.com>
2026-03-05 16:37:29 +0900

    feat: store service interface and authorization support

---
+++ src/main/java/nbcamp/TwoFastDelivery/domain/store/application/StoreService.java
@@ -0,0 +1,19 @@
+package nbcamp.TwoFastDelivery.domain.store.application;
+
+import java.util.UUID;
+public interface StoreService {
+/*     
+    UUID createStore(StoreCreateRequest request, CurrentUser user);
+
+    StoreDetailResponse getStore(UUID storeId, CurrentUser user);
+
+    void updateStore(UUID storeId, StoreUpdateRequest request, CurrentUser user);
+
+    void requestDeleteStore(UUID storeId, CurrentUser user);
+
+    Page<StoreSummaryResponse> getStores(StoreSearchCondition condition, Pageable pageable, CurrentUser user);
+
+    List<StoreSummaryResponse> getMystores(CurrentUser user);
+
+*/
+}
