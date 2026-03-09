22f2e9aa49d1ade037872de07e51baca06dd6104
ddangme <ssilver121@naver.com>
2026-03-09 10:22:13 +0900

    feat: connect AddressRequestDto messages to message bundle

--- src/main/java/nbcamp/TwoFastDelivery/domain/address/presentation/AddressRequestDto.java
+++ src/main/java/nbcamp/TwoFastDelivery/domain/address/presentation/AddressRequestDto.java
@@ -12,16 +12,16 @@ public class AddressRequestDto {
     @Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor
     public static class Create {
 
-        @NotBlank(message = "주소 별칭은 필수 입력 값입니다.")
-        @Size(max = 50, message = "별칭은 50자 이내로 입력해주세요.")
+        @NotBlank(message = "{address.alias.required}")
+        @Size(max = 50, message = "{address.alias.too_long}")
         private String alias;
 
-        @NotBlank(message = "주소는 필수 입력 값입니다.")
-        @Size(max = 255, message = "주소는 255자 이내로 입력해주세요.")
+        @NotBlank(message = "{address.address.required}")
+        @Size(max = 255, message = "{address.address.too_long}")
         private String address;
 
-        @NotBlank(message = "상세 주소는 필수 입력 값입니다.")
-        @Size(max = 255, message = "상세 주소는 255자 이내로 입력해주세요.")
+        @NotBlank(message = "{address.detail.required}")
+        @Size(max = 255, message = "{address.detail.too_long}")
         @JsonProperty("detail_address")
         private String detailAddress;
 
