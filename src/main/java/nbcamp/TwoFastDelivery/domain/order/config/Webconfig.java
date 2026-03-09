5c9bf0e34092e7b9f6413e98eec3f40d8d08d15b
kimhyerin-dev <kimhyerin.dev@gmail.com>
2026-03-09 16:24:19 +0900

    ✨ feat: 주문 API 구현

---
+++ src/main/java/nbcamp/TwoFastDelivery/domain/order/config/Webconfig.java
@@ -0,0 +1,4 @@
+package nbcamp.TwoFastDelivery.domain.order.config;
+
+public class Webconfig {
+}
---
+++ src/main/java/nbcamp/TwoFastDelivery/domain/order/security/AuthInterceptor.java
@@ -0,0 +1,28 @@
+package nbcamp.TwoFastDelivery.domain.order.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final JwtUtil jwtUtil;
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String Token = request.getHeader("Authorization");
+
+        if(Token != null && Token.startsWith("Bearer ")) {
+            String jwt = Token.substring(7);
+
+            UUID storeId = jwtUtil.getStoreIdFromToken(jwt);
+
+            request.setAttribute("storeId", storeId);
+            return true;
+        }
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증이 필요합니다.");
+        return false;
+    }
+}
