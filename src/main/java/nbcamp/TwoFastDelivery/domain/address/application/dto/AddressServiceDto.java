b8f19c01b6ae405776da709dc9f1615ec30a7187
ddangme <ssilver121@naver.com>
2026-03-09 12:06:04 +0900

    feat(address): add DTOs for address change request and response

--- src/main/java/nbcamp/TwoFastDelivery/domain/address/application/dto/AddressServiceDto.java
+++ src/main/java/nbcamp/TwoFastDelivery/domain/address/application/dto/AddressServiceDto.java
@@ -5,12 +5,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AddressServiceDto {
 
     @Getter
     @Builder
     public static class Create {
+        private String alias;
+        private String address;
+        private String detailAddress;
+    }
+
+    @Getter
+    @Builder
+    public static class Change {
+        private UUID addressId;
         private String alias;
         private String address;
         private String detailAddress;
